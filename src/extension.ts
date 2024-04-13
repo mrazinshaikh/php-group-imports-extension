cc64d0b57f547ebd66f3a6558e4731652c68864e
mrazinshaikh <razinshakh8732@gmail.com>
2024-04-13 11:06:16 +0530

    import statements order with configurable from setting added

--- src/extension.ts
+++ src/extension.ts
@@ -191,9 +191,19 @@ export function activate(context: vscode.ExtensionContext) {
 
 		// Reorder import statements based on the specified order (unchanged from previous version)
 		const orderedImports: string[] = [];
+
+		const config = vscode.workspace.getConfiguration("phpGroupImports");
+		const orderStatementsAscending = config.get('importStatementOrder') as boolean | undefined;
+
 		resolvedOrder.forEach((type, index) => {
-		  const importsOfType = groupedImports[type];
+		  let importsOfType = groupedImports[type];
 		  if (importsOfType) {
+			importsOfType = importsOfType.sort((a, b) => {
+				if (orderStatementsAscending) {
+					return a.length - b.length;
+				}
+				return b.length - a.length;
+			});
 			if (index !== resolvedOrder.length - 1) {
 				importsOfType[importsOfType.length - 1] = importsOfType[importsOfType.length - 1] + '\n';
 			}
